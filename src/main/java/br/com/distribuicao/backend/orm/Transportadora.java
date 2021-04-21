55bf7f14de3886feb12dc37234d43f2efe3eecc4
allan <allan.wylker14@gmail.com>
2021-04-21 17:39:48 -0300

    Adicionando projeto

--- src/main/java/br/com/distribuicao/backend/orm/Transportadora.java
+++ src/main/java/br/com/distribuicao/backend/orm/Transportadora.java
@@ -1,14 +1,11 @@
 package br.com.distribuicao.backend.orm;
 
-import lombok.Data;
-import lombok.ToString;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "transportadora")
-@Data
-@ToString
+
 public class Transportadora {
 
     @Id
@@ -16,4 +13,19 @@ public class Transportadora {
     private Integer codTransportadora;
     private String nome;
 
+    public Integer getCodTransportadora() {
+        return codTransportadora;
+    }
+
+    public void setCodTransportadora(Integer codTransportadora) {
+        this.codTransportadora = codTransportadora;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
