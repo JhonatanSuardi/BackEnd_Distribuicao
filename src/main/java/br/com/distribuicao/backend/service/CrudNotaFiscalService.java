b1111b65ae5cccf1a9764b9088237d3c5a758778
allan <allan.wylker14@gmail.com>
2021-05-09 12:42:06 -0300

    Request get

--- src/main/java/br/com/distribuicao/backend/service/CrudNotaFiscalService.java
+++ src/main/java/br/com/distribuicao/backend/service/CrudNotaFiscalService.java
@@ -7,12 +7,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
+@RequestMapping ("/nfe")
 public class CrudNotaFiscalService {
     @Autowired
     private NotaFiscalRepository notaFiscalRepository;
 
-    @PostMapping("/nfe") // Request com POST cai aqui.
+    @GetMapping // Mapeando verbo http(get) para quando receber a request cair nesse método e ele responde.
+    public List<NotaFiscal> Listar(){
+        return (List<NotaFiscal>) notaFiscalRepository.findAll();
+    }
+
+    @PostMapping // Request com POST cai aqui.
     @ResponseStatus(HttpStatus.CREATED)
     public void adicionar(@RequestBody NotaFiscalDTO notaFiscalDTO){
         System.out.println(notaFiscalDTO);
