1a5142836abb944efb7bbcefeb2e33793ff0aa66
allan <allan.wylker14@gmail.com>
2021-05-09 20:29:33 -0300

    atualização crudnfe

--- src/main/java/br/com/distribuicao/backend/service/CrudNotaFiscalService.java
+++ src/main/java/br/com/distribuicao/backend/service/CrudNotaFiscalService.java
@@ -5,6 +5,7 @@ import br.com.distribuicao.backend.orm.NotaFiscal;
 import br.com.distribuicao.backend.repository.NotaFiscalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,5 +26,38 @@ public class CrudNotaFiscalService {
     public void adicionar(@RequestBody NotaFiscalDTO notaFiscalDTO){
         System.out.println(notaFiscalDTO);
         notaFiscalRepository.save(NotaFiscal.valueOf(notaFiscalDTO));
+    }
+    @GetMapping(path = {"/cod_nfe"})
+    public ResponseEntity findById(@PathVariable int cod_nfe){
+        return notaFiscalRepository.findById(cod_nfe)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+    @PutMapping(value="/{cod_nfe}")
+    public ResponseEntity update(@PathVariable("cod_nfe") int cod_nfe,
+                                 @RequestBody NotaFiscal notaFiscal) {
+        return notaFiscalRepository.findById(cod_nfe)
+                .map(record -> {
+                    record.setMotorista(notaFiscal.getMotorista());
+                    record.setCodPlanta(notaFiscal.getCodPlanta());
+                    record.setCodRegistro(notaFiscal.getCodRegistro());
+                    record.setTransportadora(notaFiscal.getTransportadora());
+                    record.setNumNfe(notaFiscal.getNumNfe());
+                    record.setNumSerie(notaFiscal.getNumSerie());
+                    record.setDocTransporte(notaFiscal.getDocTransporte());
+                    record.setPlaca(notaFiscal.getPlaca());
+                    record.setPerfilCarga(notaFiscal.getPerfilCarga());
+                    record.setEstado(notaFiscal.getEstado());
+                    NotaFiscal updated = notaFiscalRepository.save(record);
+                    return ResponseEntity.ok().body(updated);
+                }).orElse(ResponseEntity.notFound().build());
+    }
+    @DeleteMapping(path ={"/{cod_nfe}"})
+    public ResponseEntity <?> delete(@PathVariable int cod_nfe) {
+        return notaFiscalRepository.findById(cod_nfe)
+                .map(record -> {
+                    notaFiscalRepository.deleteById(cod_nfe);
+                    return ResponseEntity.ok().build();
+                }).orElse(ResponseEntity.notFound().build());
     }
 }
