3c65e9f0b4f3cee54487da33234da5d90798cc55
JHONATANOLIVEIRASUAR <jhonatan.suardi123@gmail.com>
2021-05-12 21:51:22 -0300

    Adicionando Find all

--- src/main/java/br/com/distribuicao/backend/service/CrudNotaFiscalService.java
+++ src/main/java/br/com/distribuicao/backend/service/CrudNotaFiscalService.java
@@ -3,14 +3,10 @@ package br.com.distribuicao.backend.service;
 import br.com.distribuicao.backend.dto.NotaFiscalDTO;
 import br.com.distribuicao.backend.orm.NotaFiscal;
 import br.com.distribuicao.backend.repository.NotaFiscalRepository;
-import org.aspectj.weaver.ast.Not;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
 
 
 @RestController
@@ -22,16 +18,21 @@ public class CrudNotaFiscalService {
 
         @GetMapping(path = "/{codNfe}") // Mapeando verbo http(get) para quando receber a request cair nesse método e ele responde.
         @ResponseStatus(HttpStatus.OK)
-        public ResponseEntity<NotaFiscal> Listar(@PathVariable String codNfe){
+        public ResponseEntity<NotaFiscal> findById(@PathVariable String codNfe){
             return notaFiscalRepository.findById(codNfe)
                     .map(record -> ResponseEntity.ok().body(record))
                     .orElse(ResponseEntity.notFound().build());
         }
 
+        @GetMapping
+        @ResponseStatus(HttpStatus.OK)
+        public Iterable<NotaFiscal> findAll() {
+            return notaFiscalRepository.findAll();
+        }
+
         @PostMapping // Request com POST cai aqui.
         @ResponseStatus(HttpStatus.CREATED)
         public void adicionar(@RequestBody NotaFiscalDTO notaFiscalDTO){
-            System.out.println(notaFiscalDTO);
             notaFiscalRepository.save(NotaFiscal.valueOf(notaFiscalDTO));
         }
 
